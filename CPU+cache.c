8017b108e7e4d5f7f36218ba99b901ddbe5c892f
Woodrow Fulmer <neomagam@gmail.com>
2017-11-01 17:48:51 -0400

    How to count misses without branch prediction

--- CPU+cache.c
+++ CPU+cache.c
@@ -44,16 +44,16 @@ int main(int argc, char **argv)
   unsigned int cycle_number = 0;
 
   if (argc == 2) {
-    //Assume trace = 0
-    
+	//Assume trace = 0
+	
   }
   else if (argc == 3) trace_view_on = atoi(argv[2]);
   else {
- 	fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - (0/1)>\n");
-    fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
-    exit(0);
+	fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - (0/1)>\n");
+	fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
+	exit(0);
   }
-    
+	
   trace_file_name = argv[1];
 
   fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
@@ -61,8 +61,8 @@ int main(int argc, char **argv)
   trace_fd = fopen(trace_file_name, "rb");
 
   if (!trace_fd) {
-    fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
-    exit(0);
+	fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
+	exit(0);
   }
   
   trace_init();
@@ -73,8 +73,8 @@ int main(int argc, char **argv)
   config_fd = fopen("cache_config.txt", "r");
   
   if (!config_fd) {
-     fprintf(stdout, "\nconfig file not opened.\n\n");
-    exit(0);
+	 fprintf(stdout, "\nconfig file not opened.\n\n");
+	exit(0);
   }
 
   //read config file
@@ -90,18 +90,18 @@ int main(int argc, char **argv)
 //Begin pipeline computation
   while(1) {
   
-  	// Check for lw hazard DEBUG: should these be detected in the ID_EX buffer? DEBUG1
-  	if( (EX_MEM.type == 3) && ((EX_MEM.dReg == ID_EX.sReg_a) || (EX_MEM.dReg == ID_EX.sReg_b)))
-  	{
-  		*fetch_entry = IF_ID;
-  		
-  		IF_ID = ID_EX;
-  		
-  		ID_EX.type = 0;		
-   	}
-   	else
-   	{
-   	   	if( EX_MEM.type == 5) //branch
+	// Check for lw hazard DEBUG: should these be detected in the ID_EX buffer? DEBUG1
+	if( (EX_MEM.type == 3) && ((EX_MEM.dReg == ID_EX.sReg_a) || (EX_MEM.dReg == ID_EX.sReg_b)))
+	{
+		*fetch_entry = IF_ID;
+		
+		IF_ID = ID_EX;
+		
+		ID_EX.type = 0;		
+	}
+	else
+	{
+		if( EX_MEM.type == 5) //branch
 		{
 			//Check PC of Branch with instruction in ID buffer
 			unsigned int b_pc, id_pc;
@@ -125,17 +125,17 @@ int main(int argc, char **argv)
 			}
 		}
 		
-    	size = trace_get_item(&fetch_entry);
-   	}
+		size = trace_get_item(&fetch_entry);
+	}
    
-    if (cycle_number >= stop) {       /* no more instructions (trace_items) to simulate */
-        printf("+ Simulation terminates at cycle : %u\n", cycle_number);
-        printf("I-cache accesses %u and misses %u\n", I_accesses, I_misses);
-      	printf("D-cache Read accesses %u and misses %u\n", D_read_accesses, D_read_misses);
-      	printf("D-cache Write accesses %u and misses %u\n", D_write_accesses, D_write_misses);
-      	break;
-    }
-    else{           
+	if (cycle_number >= stop) {       /* no more instructions (trace_items) to simulate */
+		printf("+ Simulation terminates at cycle : %u\n", cycle_number);
+		printf("I-cache accesses %u and misses %u\n", I_accesses, I_misses);
+		printf("D-cache Read accesses %u and misses %u\n", D_read_accesses, D_read_misses);
+		printf("D-cache Write accesses %u and misses %u\n", D_write_accesses, D_write_misses);
+		break;
+	}
+	else{           
 		no_print = 0;
 		
 		//Propagate in reverse for simplicity
@@ -165,12 +165,12 @@ int main(int argc, char **argv)
 			
 		cycle_number++;	
 	int j;
-      	for(j = 0 ; j < 3 ; j++)
-      	{
-      		if(squashed[j] == tr_entry->PC && cycle_number >= 5 && !end) //DEBUG ugly condition, consider commenting
-      		{
-      			if(trace_view_on)
-      			{
+		for(j = 0 ; j < 3 ; j++)
+		{
+			if(squashed[j] == tr_entry->PC && cycle_number >= 5 && !end) //DEBUG ugly condition, consider commenting
+			{
+				if(trace_view_on)
+				{
 					printf("[cycle %d] BRANCH:",cycle_number) ;
 		      		printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
 					printf("[cycle %d] SQUASHED\n",++cycle_number);
@@ -182,63 +182,82 @@ int main(int argc, char **argv)
 				}
 				squashed[j] = 0;
 				no_print = 1;
-      		}
-      	}	
-    }  
-    int temp = cycle_number;
-    cycle_number = cycle_number + cache_access(I_cache, tr_entry->PC, 0); /* simulate instruction fetch */
+			}
+		}	
+	}  
+	int temp = cycle_number;
+	cycle_number = cycle_number + cache_access(I_cache, tr_entry->PC, 0); /* simulate instruction fetch */
 	// update I_access and I_misses
 	I_accesses++;
 	if(cycle_number > temp) I_misses++;
-    if (trace_view_on && !no_print && cycle_number >= 5) {/* print the executed instruction if trace_view_on=1 and don't print the first cycle's initial value*/
-      switch(tr_entry->type) {
-        case ti_NOP:
-          printf("[cycle %d] NOP:\n",cycle_number) ;
-          break;
-        case ti_RTYPE:
-          printf("[cycle %d] RTYPE:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
-          break;
-        case ti_ITYPE:
-          printf("[cycle %d] ITYPE:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
-          break;
-        case ti_LOAD:
-          printf("[cycle %d] LOAD:",cycle_number) ;      
-          printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
-          temp = cycle_number;
-          cycle_number = cycle_number + cache_access(D_cache, tr_entry->Addr, 0);
+	if (trace_view_on && !no_print && cycle_number >= 5) {/* print the executed instruction if trace_view_on=1 and don't print the first cycle's initial value*/
+	  switch(tr_entry->type) {
+		case ti_NOP:
+		  printf("[cycle %d] NOP:\n",cycle_number) ;
+		  break;
+		case ti_RTYPE:
+		  printf("[cycle %d] RTYPE:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(dReg: %d) \n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->dReg);
+		  break;
+		case ti_ITYPE:
+		  printf("[cycle %d] ITYPE:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+		  break;
+		case ti_LOAD:
+		  printf("[cycle %d] LOAD:",cycle_number) ;      
+		  printf(" (PC: %x)(sReg_a: %d)(dReg: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->dReg, tr_entry->Addr);
+		  temp = cycle_number;
+		  cycle_number = cycle_number + cache_access(D_cache, tr_entry->Addr, 0);
 		  // update D_read_access and D_read_misses
 		  D_read_accesses++;
 		  if(cycle_number > temp) I_misses++;
-          break;
-        case ti_STORE:
-          printf("[cycle %d] STORE:",cycle_number) ;      
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
-          temp = cycle_number;
+		  break;
+		case ti_STORE:
+		  printf("[cycle %d] STORE:",cycle_number) ;      
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+		  temp = cycle_number;
 		  cycle_number = cycle_number + cache_access(D_cache, tr_entry->Addr, 1);
 		  // update D_write_access and D_write_misses 
 		  D_write_accesses++;  
 		  if(cycle_number > temp) I_misses++;       
-          break;
-        case ti_BRANCH:
-          printf("[cycle %d] BRANCH:",cycle_number) ;
-          printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
-          break;
-        case ti_JTYPE:
-          printf("[cycle %d] JTYPE:",cycle_number) ;
-          printf(" (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
-          break;
-        case ti_SPECIAL:
-          printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
-          break;
-        case ti_JRTYPE:
-          printf("[cycle %d] JRTYPE:",cycle_number) ;
-          printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
-          break;
-      }
+		  break;
+		case ti_BRANCH:
+		  printf("[cycle %d] BRANCH:",cycle_number) ;
+		  printf(" (PC: %x)(sReg_a: %d)(sReg_b: %d)(addr: %x)\n", tr_entry->PC, tr_entry->sReg_a, tr_entry->sReg_b, tr_entry->Addr);
+		  break;
+		case ti_JTYPE:
+		  printf("[cycle %d] JTYPE:",cycle_number) ;
+		  printf(" (PC: %x)(addr: %x)\n", tr_entry->PC,tr_entry->Addr);
+		  break;
+		case ti_SPECIAL:
+		  printf("[cycle %d] SPECIAL:\n",cycle_number) ;      	
+		  break;
+		case ti_JRTYPE:
+		  printf("[cycle %d] JRTYPE:",cycle_number) ;
+		  printf(" (PC: %x) (sReg_a: %d)(addr: %x)\n", tr_entry->PC, tr_entry->dReg, tr_entry->Addr);
+		  break;
+	  }
 
-    }
+	}
+	else if(cycle_number >= 5)
+	{
+		if(tr_entry->type == ti_LOAD)
+		{
+			temp = cycle_number;
+			cycle_number = cycle_number + cache_access(D_cache, tr_entry->Addr, 0);
+			// update D_read_access and D_read_misses
+			D_read_accesses++;
+			if(cycle_number > temp) I_misses++;
+		}
+		else if (tr_entry->type == ti_STORE)
+		{
+		  temp = cycle_number;
+		  cycle_number = cycle_number + cache_access(D_cache, tr_entry->Addr, 1);
+		  // update D_write_access and D_write_misses 
+		  D_write_accesses++;  
+		  if(cycle_number > temp) I_misses++;   
+		}
+	}
   }
 
   trace_uninit();
